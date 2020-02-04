6f859f44e53b8102676e7fc5406736ca3da17bcc
Kapeel Kokane <kapeel.kokane@gmail.com>
2020-02-04 20:04:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chart1 from './Chart1';
+import Chart2 from './Chart2';
+
+const data1 = [12, 36, 15, 25, 35, 10, 40];
+const data2 = [5, 10, 15, 20, 25, 30];
+const data3 = [10, 30, 10, 5];
+
+const w = 500;
+const h = 400;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Chart2 data={data1} w={w} h={h} color="green"/>
+        <Chart2 data={data2} w={w} h={h} color="orange"/>
+        <Chart2 data={data3} w={w} h={h} color="cyan"/>
+        <Chart2 data={data3} w={w} h={h} color="tomato"/>
       </header>
     </div>
   );
